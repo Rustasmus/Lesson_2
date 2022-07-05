ebf72020e1ced6a3aa8076f25807cbe76e90419d
git <git>
2022-07-06 00:27:27 +0600

    satrt

---
+++ lesson_class_work.py
@@ -0,0 +1,53 @@
+# lesson_one = 2
+# lesson_two = '2'
+# lesson_three = 3.0
+# lesson_four = False
+# variables \ data type
+
+# int() Преобразование в целоисленное значение
+# float() - Преобразивание в дробное значение
+# str() - Преобразовать в строку
+
+a = float(input('Enter first number: '))
+b = float(input('Enter second number: '))
+sum_ = a + b
+minus = a - b
+divide = a / b
+multiply = a * b
+exponentiation = a ** b
+division_by_modulus = a // b
+remains_from_division = a % b
+
+
+
+bigger = a > b
+losser = a < b
+bigger_or_equal = a >= b
+lossee_or_equal = a <= b
+equal = a == b
+not_equal = a != b
+
+# first_number = input('Enter first number: ')
+# second_number = input('Enter second number: ')
+
+# print( first_number )
+# print( second_number )
+
+print(a)
+print(b)
+print(sum_)
+print(minus)
+print(divide)
+print(multiply)
+print(exponentiation)
+print(division_by_modulus)
+print(remains_from_division)
+print(bigger)
+print(losser)
+print(bigger_or_equal)
+print(lossee_or_equal)
+print(equal)
+print(not_equal)
+
+
+
